ec5fa85b27b1508ebc7724f8d33beb82316cebc7
Mounir Ait Sliman <mo.aitsliman@gmail.com>
2025-01-06 18:50:31 +0100

    aadding annimation using framer-motion

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import clsx from "clsx";
-const ItemLayout = ({ children, className }) => {
-  return (
-    <div
-      className={clsx(
-        " cusom-bg p-6 sm:p-8 rounded-xl flex items-center justify-center space-y-8",
-        className
-      )}
-    >
-      {children}
-    </div>
-  );
-};
+import ItemLayout from "./ItemLayout";
+
 const AboutDetails = () => {
   return (
     <section className="py-20 w-full">
--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -3,7 +3,21 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import emailjs from "@emailjs/browser";
 import { Toaster, toast } from "sonner";
-
+import { motion } from "framer-motion";
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+    },
+  },
+};
+const item = {
+  hidden: { scale: 0 },
+  show: { scale: 1 },
+};
 export default function Form() {
   const {
     register,
@@ -57,11 +71,15 @@ export default function Form() {
   return (
     <>
       <Toaster richColors={true} />
-      <form
+      <motion.form
+        variants={container}
+        initial="hidden"
+        animate="show"
         onSubmit={handleSubmit(onSubmit)}
         className="max-w-md w-full flex flex-col items-center justify-center space-y-4"
       >
-        <input
+        <motion.input
+          variants={item}
           className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cusom-bg"
           type="text"
           name="Full_Name"
@@ -79,7 +97,8 @@ export default function Form() {
             {errors.Full_Name.message}
           </span>
         )}
-        <input
+        <motion.input
+          variants={item}
           className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cusom-bg"
           type="email"
           placeholder="Email"
@@ -90,7 +109,8 @@ export default function Form() {
             {errors.Email.message}
           </span>
         )}
-        <textarea
+        <motion.textarea
+          variants={item}
           placeholder="Message"
           className="w-full p-2 rounded-md shadow-lg text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cusom-bg"
           {...register("Message", {
@@ -111,12 +131,13 @@ export default function Form() {
           </span>
         )}
 
-        <input
+        <motion.input
+          variants={item}
           value="Send your message"
           className="px-10 py-4 rounded-md shadow-lg bg-background border border-accent/30 border-solid hover:shadow-glass-sm backdrop-blur-sm text-foreground focus:outline-none focus:ring-2 focus:ring-accent/50 cursor:pointer capitalize"
           type="submit"
         />
-      </form>
+      </motion.form>
     </>
   );
 }
