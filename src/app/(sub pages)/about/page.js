f4e6836dce9211cff85b4cd91648c36a6a4ecdbf
Mounir Ait Sliman <mo.aitsliman@gmail.com>
2025-01-06 13:23:52 +0100

    mobile responsive for the about and layout pages

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -12,14 +12,16 @@ export default function Home() {
         alt="background-image"
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
-      <div className="w-full h-screen absolute top-1/2 -translate-y-1/2 left-0">
+      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0">
         <RenderModel>
           <Hat />
         </RenderModel>
       </div>
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
-          <h1 className="font-bold text-7xl text-accent ">Mounir Ait Sliman</h1>
+        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+          <h1 className="font-bold text-4xl xs:text-5xl sm:text-6xl lg:text-7xl text-accent ">
+            Mounir Ait Sliman
+          </h1>
           <p className="font-light text-foreground text-ls">
             Meet the wizard behind this portfolio
           </p>
