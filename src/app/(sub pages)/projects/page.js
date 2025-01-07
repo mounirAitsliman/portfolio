fb697d9574095ddbb46b3ddab93f64a820c5d480
Mounir Ait Sliman <mo.aitsliman@gmail.com>
2025-01-07 13:13:48 +0100

    updating config,bg,resume

--- src/app/(sub pages)/projects/page.js
+++ src/app/(sub pages)/projects/page.js
@@ -3,11 +3,11 @@ import bg from "../../../../public/background/projects-background.png";
 import ProjectList from "@/components/projects";
 import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
-// import { Staff } from "@/components/models/Staff";
-import dynamic from "next/dynamic";
-const Staff = dynamic(() => import("@/components/models/Staff"), {
-  ssr: false,
-});
+import { Staff } from "@/components/models/Staff";
+// import dynamic from "next/dynamic";
+// const Staff = dynamic(() => import("@/components/models/Staff"), {
+//   ssr: false,
+// });
 export default function Home() {
   return (
     <>
@@ -16,7 +16,7 @@ export default function Home() {
         sizes="100vw"
         src={bg}
         alt="background-image"
-        className="-z-50 fixed top-0 -left-24 w-full h-full object-cover object-center opacity-25"
+        className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
       <ProjectList projects={projectsData} />
       <div className="flex items-center justify-center fixed top-16 lg:top-20 left-1/2 -translate-x-1/2 lg:translate-x-0 -z-10 lg:-left-24 h-screen">
